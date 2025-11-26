de0564b9e2e4d70799200d1379b5718a23b6eb39
Thinn994 <thinh3dieu@gmail.com>
2025-11-26 18:56:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import ast
 import tempfile
 from datetime import datetime
 import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_mail import Mail, Message   # nếu dùng mail
 from werkzeug.security import generate_password_hash, check_password_hash
 import google.generativeai as genai
@@ -1618,5 +1618,6 @@ def google_search(query):
 # === KHỞI CHẠY APP ===
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
